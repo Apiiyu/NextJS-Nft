af762384fb4f288debe34bc98944ddfaba0bb192
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-11-01 14:54:25 +0700

    chore: update ignore files and scripts on package.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,64 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "regal-blue": "#243c5a",
+        "nft-dark": "#24252D",
+        "nft-gray-1": "#E3E1E3",
+        "nft-gray-2": "#888888",
+        "nft-gray-3": "#4F4F4F",
+        "nft-black-1": "#2D2E36",
+        "nft-black-2": "#1B1A21",
+        "nft-black-3": "#2A2D3A",
+        "nft-black-4": "#24252D",
+        "nft-red-violet": "#DA18A3",
+        "file-active": "#2196f3",
+        "file-accept": "#00e676",
+        "file-reject": "#ff1744",
+        "overlay-black": "rgba(0, 0, 0, 0.8)",
+      },
+      inset: {
+        45: "45%",
+        65: "65px",
+      },
+      spacing: {
+        65: "65px",
+      },
+      flex: {
+        2: "2 2 0%",
+      },
+      lineHeight: {
+        70: "70px",
+      },
+      zIndex: {
+        "-5": "-5",
+        0: "0",
+      },
+      screens: {
+        lg: { max: "1800px" },
+        md: { max: "990px" },
+        sm: { max: "600px" },
+        xs: { max: "400px" },
+        minmd: "1700px",
+        minlg: "2100px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
